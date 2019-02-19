42f070587ca28295ff1c680e7596480576557fbc
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2019-02-19 11:39:39 +0100

    Added a few additional tests.

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(SurveyExactDataImporter)
+
+test_check("SurveyExactDataImporter")
--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -2,9 +2,19 @@ library(testthat)
 library(SurveyExactDataImporter)
 
 context("SurveyExactDataImporter")
-# test_check("SurveyExactDataImporter")
 
 
-# Load example dataset
-data <- load_survey_exact_data("../../data/dataset_1488637_20180512_1825827966544069436.xlsx")
+test_that('Data can be loaded', {
+  expect_is(load_survey_exact_data("../../data/dataset_1488637_20180512_1825827966544069436.xlsx"), 'list')
+  expect_error(load_survey_exact_data("file_that_does_not_exist.xlsx"))
+})
 
+test_that('Variable descriptions can be reached', {
+  data <- load_survey_exact_data("../../data/dataset_1488637_20180512_1825827966544069436.xlsx")
+  expect_equal(get_variable_name(data, 's_6'),
+               'Question battery - Element one')
+  expect_equal(get_variable_name_category(data, 's_6'),
+               'Question battery')
+  expect_equal(get_variable_name_subcategory(data, 's_6'),
+               'Element one')
+})
